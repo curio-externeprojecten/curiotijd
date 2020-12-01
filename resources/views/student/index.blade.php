a341863c1d5d235e445b73dec79476a109351eec
duncanVerdult <63355499+duncanVerdult@users.noreply.github.com>
2020-12-01 11:07:29 +0100

    dashboard concept

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -1,1 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Dashboard</h1>
+<div class="row">
+    <div class="badges col-5">
+        <h2>Behaalde badges:</h2>
+        <img src="https://i.ibb.co/4fYy1gN/image.png" alt="Curio">
+        <p>Help, ik zit op curio</p>
+    </div>
+    <div class="challenges col-5">
+        <h2>Challenges:</h2>
+        <img src="" alt="">
+        <p>Geen challenges beschikbaar</p>
+    </div>
+    <div class="gegevens col-2">
+        <h2>Gegevens</h2>
+        <p>Naam:</p>
+        <p>Achternaam:</p>
+        <p>Klas:</p>
+        <p>Mentor:</p>
+        <p>Leerjaar:</p>
+    </div>
+</div>
+@endsection
+
 
