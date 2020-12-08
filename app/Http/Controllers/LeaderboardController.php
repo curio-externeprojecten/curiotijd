58bceaa7c24caf2664341b456e129ce20a45083c
Ravithiele <63392140+Ravithiele@users.noreply.github.com>
2020-12-08 10:46:01 +0100

    leaderboard

--- app/Http/Controllers/LeaderboardController.php
+++ app/Http/Controllers/LeaderboardController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class LeaderboardController extends Controller
 {
     public function index(){
-        $students = \DB::table('students')->get();
+        $students = \DB::table('students')->take(10)->get();
+        // $achievements = \DB::table('achievements')->orderBy('id', 'DESC');
         return view('leaderboard.index' , ['students' => $students]);
     }
 }
--- resources/views/leaderboard/index.blade.php
+++ resources/views/leaderboard/index.blade.php
@@ -1,6 +1,8 @@
 <h1>Leaderboard</h1>
 @foreach($students as $student)
-<p>voornaam: {{$student->first_name}}</p>
-<p>klas: {{$student->klas_id}}</p>
+<p>Naam: {{$student->first_name}} {{$student->last_name}}</p>
+{{-- <p>Badges: {{$achievements->id}}</p> --}}
+<p>Klas: {{$student->klas_id}}</p>
 <hr>
 @endforeach
+{{-- <button><p>Leaderboard uit</p></button> --}}
